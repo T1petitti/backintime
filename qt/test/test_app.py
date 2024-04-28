d085457828b5d71ab70a8a8a50637363fff65dc4
vozella <vozellad@protonmail.com>
2024-04-28 18:28:47 -0400

    Fix typo. Add comments.

--- qt/test/test_app.py
+++ qt/test/test_app.py
@@ -54,16 +54,13 @@ class TestMainWindow(unittest.TestCase):
         self.appInstance.exitApplication()
         logger.closelog()
 
-        del self.cfg
-        del self.appInstance
-        del self.mainWindow
-
     def add_default_prefs(self, list_to_append):
+        """Add default preferences to list."""
         for key, val in self.default_prefs.items():
             list_to_append.setdefault(key, val)
 
     def test_get_preferences_when_file_exists_and_no_prefs(self):
-        """When file doesn't have preference"""
+        """When file doesn't have preferences."""
         self.mainWindow.main_preferences = self.test_prefs
         self.mainWindow.save_preferences(self.test_prefs_file_name)
         prefs = self.mainWindow.get_preferences(self.test_prefs_file_name)
@@ -71,7 +68,7 @@ class TestMainWindow(unittest.TestCase):
         self.assertEqual(prefs, self.mainWindow.main_preferences)
 
     def test_get_preferences_when_file_exists_and_has_prefs(self):
-        """When file has preference"""
+        """When file has preferences."""
         self.mainWindow.main_preferences = self.test_prefs
         self.add_default_prefs(self.mainWindow.main_preferences)
         self.mainWindow.save_preferences(self.test_prefs_file_name)
@@ -79,6 +76,7 @@ class TestMainWindow(unittest.TestCase):
         self.assertEqual(prefs, self.mainWindow.main_preferences)
 
     def test_get_preferences_when_file_does_not_exist(self):
+        """Ensure file gets created if it doesn't exist."""
         prefs = self.mainWindow.get_preferences('non_existent_file.json')
         self.assertEqual(prefs, self.default_prefs)
 
@@ -89,42 +87,51 @@ class TestMainWindow(unittest.TestCase):
         self.assertEqual(prefs, self.default_prefs)
 
     def test_get_preferences_with_invalid_json(self):
+        """When preferences data in file is invalid."""
         with open(self.test_prefs_file_name, 'w') as file:
             file.write("Invalid JSON data")
         prefs = self.mainWindow.get_preferences(self.test_prefs_file_name)
         self.assertEqual(prefs, self.default_prefs)
 
     def test__verify_preferences_when_prefs_is_none(self):
+        """When argument is None."""
         prefs = self.mainWindow._verify_preferences(None)
         self.assertEqual(prefs, self.default_prefs)
 
     def test__verify_preferences_when_no_prefs(self):
+        """When argument is empty."""
         verified_prefs = self.mainWindow._verify_preferences({})
         self.assertEqual(verified_prefs, self.default_prefs)
 
     def test__verify_preferences_when_empty_list_arg(self):
+        """When argument is an empty list."""
         verified_prefs = self.mainWindow._verify_preferences([])
         self.assertEqual(verified_prefs, self.default_prefs)
 
     def test__verify_preferences_when_list_arg(self):
+        """When argument is a list."""
         with self.assertRaises(TypeError):
             self.mainWindow._verify_preferences([True])
 
-    def test_verify_preferences_when_has_prefs(self):
-        verified_prefs = self.mainWindow.verify_preferences(self.default_prefs)
+    def test__verify_preferences_when_has_prefs(self):
+        """Ensure correct data comes back unchanged."""
+        verified_prefs = self.mainWindow._verify_preferences(self.default_prefs)
         self.assertEqual(verified_prefs, self.default_prefs)
 
-    def test_verify_preferences_when_has_extra_prefs(self):
+    def test__verify_preferences_when_has_extra_prefs(self):
+        """Ensure preferences remain in data."""
         prefs = self.default_prefs
         prefs['extra_key'] = 'extra_value'
-        verified_prefs = self.mainWindow.verify_preferences(prefs)
+        verified_prefs = self.mainWindow._verify_preferences(prefs)
         self.assertEqual(verified_prefs, prefs)
 
     def test_save_preferences_writes_file(self):
+        """Ensure file gets created."""
         self.mainWindow.save_preferences(self.test_prefs_file_name)
         self.assertTrue(os.path.exists(self.test_prefs_file_name))
 
     def test_save_preferences_writes_correct_data(self):
+        """Ensure preferences gets saved correctly."""
         self.mainWindow.main_preferences = self.test_prefs
         self.mainWindow.save_preferences(self.test_prefs_file_name)
         with open(self.test_prefs_file_name, 'r') as file:
